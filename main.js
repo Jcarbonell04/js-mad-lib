961929b22adb12995acc128615004417db3fe803
Jcarbonell04 <61067221+Jcarbonell04@users.noreply.github.com>
2020-03-04 15:26:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,8 @@ function generateMadlib() {
     let adjective6 = document.getElementById('adj6').value; 
     let pluralNoun3 = document.getElementById('plural-noun3').value;
 
-
     //Build Message
-    let MadLib = ;
+    let MadLib = "Most of the time " + adjective + ' ' + "teachers always give out " + adjective2 + " assignments. But as everyone knows, if you want to pass all your classes so you can go to a/an " + noun + " and become president of a big international " + noun2 + " and have millions of " + pluralNoun + " in the bank, you must do your homework and study " + adverb + ". If you just sit around and " + verb + ", you won't get ahead in life. You must learn to pay attention to every " + adjective3 + " thing your teacher says. Do not interrupt or whisper to other " + pluralNoun2 + " during class. Be sure to have a nice, " + adjective4 + " notebook in which you can write down anything the teacher says that seems " + adjective5 + '. go home and memorize all of those ' + adjective6 + " notes. When your teacher gives a surprise quiz, you will know all of the " + pluralNoun3 + "!";
 
     //Output the Message
     document.getElementById('output').innerHTML = MadLib;
